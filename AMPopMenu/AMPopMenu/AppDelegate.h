082e962a16deb3abdf56beebcdb3eb4f96d582e0
donbe <donbe520@gmail.com>
2015-01-26 18:48:45 +0800

    init

---
+++ AMPopMenu/AMPopMenu/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AMPopMenu
+//
+//  Created by renhe on 15/1/26.
+//  Copyright (c) 2015年 donbe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AMPopMenu/AMPopMenu/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AMPopMenu
+//
+//  Created by renhe on 15/1/26.
+//  Copyright (c) 2015年 donbe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
